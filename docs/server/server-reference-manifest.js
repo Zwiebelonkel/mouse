945f86b90f7a0a47faffc588ae597531091bab3b
Luca Müller <lucamuller2004@gmail.com>
2025-11-13 17:13:05 +0000

    electron

--- docs/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pd6nhZ8Y7EwgZRZByNDlRW+9dKp5bxpFs0Tq9dnW9Gs=\"\n}"
--- docs/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_9124df7d._.js"
-],
-    source: "dynamic"
-});

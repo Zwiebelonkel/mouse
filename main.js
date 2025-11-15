e131a1223811e9d8c4a5ff52e507c5c2d16d91dc
MARINESOFT\mueller_ja <jan-luca.mueller@benntec.de>
2025-11-15 12:25:05 +0100

    Electron

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
+  const isDev = !app.isPackaged;
+
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
+    icon: path.join(__dirname, "icons/app.ico"), // ICON hier
   });
 
   win.loadURL(
     isDev
-      ? 'http://localhost:9003'
-      : `file://${path.join(__dirname, 'docs/index.html')}`
+      ? "http://localhost:9003"
+      : `file://${path.join(__dirname, "docs/index.html")}`
   );
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
